12d541916751e3148258610f42f4a0fabdf2b78f
schaabane <chaabaneseifeddine@gmail.com>
2025-02-13 08:02:59 +0100

    main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,56 @@
-import './layout.scss'
-import Navbar from './components/navbars/Navbar.jsx'
-import HomePage from './Routes/homePage/homePage.jsx'
+import HomePage from './Routes/homePage/homePage.jsx';
+import ListPage from "./Routes/listPage/listPage.jsx";
+import Layout from "./Routes/layout/layout.jsx";
+import SinglePage from "./Routes/singlePage/singlePage.jsx";
+//import ProfilePage from "./Routes/profilePage/profilePage.jsx";
+import Login from "./Routes/login/login.jsx";
+//import Register from "./Routes/register/register.jsx";
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
 
 function App() {
+
+
+  // Create the Router
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        },
+        // {
+        //   path:"/profile",
+        //   element:<ProfilePage/>
+        // },
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        // {
+        //   path:"/register",
+        //   element:<Register/>
+        // }
+      ]
+    }
+  ]);  
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage/>
-      </div>
-    </div>
-  )
+    <RouterProvider router={router}/>
+  );
 }
 
-export default App
+export default App;
